db6f968e37d7fbfca07c244cc791d77b318893de
soedrego <soedrego@gmail.com>
2017-10-22 11:16:33 +0200

    Último cambio en el archivo main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var mes_text = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var mes_text = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 var dia_text = ["Dom", "Lun", "Mar", "Mie", "Juv", "Vie", "Sab"];
 
