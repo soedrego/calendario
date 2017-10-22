81dedde9359653455a66786d35b1c540656bf6d2
soedrego <soedrego@gmail.com>
2017-10-22 11:55:59 +0200

    completando el main.js para ver la web

--- main.js
+++ main.js
@@ -27,4 +27,42 @@ function estructurar() {
      cabecera.appendChild(fila);
  
  for (d = 0; d < 7; d++) {
- 		
+ 	let dia = document.createElement("TH");
+ 	dia.innerText = dia_text[d];
+ 	fila.appendChild(dia);
+ 	}
+
+ 	//Cuerpo
+
+ 	 let cuerpo = document.createElement("TBODY");
+ 	 tabla_mes.appendChild(cuerpo);
+ 	 for (f = 0; f < 6; f++) {
+ 		 let fila = document.createElement("TR");
+ 	   cuerpo.appendChild(fila);
+ 		 for (d = 0; d < 7; d++) {
+ 	   	let dia = document.createElement("TD");
+ 	    dia.innerText = "";
+ 	    fila.appendChild(dia);
+ 	 		 }     
+ 		 }    
+	 }
+ }
+ numerar();
+
+ function numerar() {
+   for (i = 1; i < 366; i++) {
+  	 let fecha = fechaPorDia(2017, i);
+     let mes = fecha.getMonth();
+     let select_tabla = document.getElementsByClassName('tabla_mes')[mes];
+     let dia = fecha.getDate()
+     let dia_semana = fecha.getDay();
+     if (dia == 1) {var sem = 0;}
+ select_tabla.children[2].children[sem].children[dia_semana].innerText = dia;
+     if (dia_semana == 6) { sem = sem + 1; }
+		 }
+	 }
+
+function fechaPorDia(año, dia) {
+	 var date = new Date(año, 0);
+   return new Date(date.setDate(dia));
+ }
